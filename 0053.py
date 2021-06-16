18af7b7513e3f0478546cec2c897e81515aff1cb
X <xieweijunx@gmail.com>
2021-06-17 00:14:49 +0800

    Update 0053.py

--- 0053.py
+++ 0053.py
@@ -1,7 +1,27 @@
 # 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 from typing import List
 class Solution:
+    #2021/06/17
     def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        maxSum = nums[0]
+        cntSum = nums[0]
+
+        for val in nums[1:]:
+            tmp = cntSum+val
+            if tmp > val:
+                cntSum = tmp
+            else:
+                cntSum = val
+
+            if cntSum > maxSum:
+                maxSum = cntSum
+
+        return maxSum
+    #old
+    def maxSubArray1(self, nums: List[int]) -> int:
         # min value
         maxVal = nums[0]
         collectingMax = maxVal
